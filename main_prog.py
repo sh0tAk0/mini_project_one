e6df6355c8e7abc9fc45e5ce669bd28b778dadbe
mohamadreza <mohamad.shabro1382@gmail.com>
2023-04-16 16:50:35 +0330

    update check function

--- main_prog.py
+++ main_prog.py
@@ -3,14 +3,14 @@ from time import sleep
 from os import name , system
 
 def clear():
-   """clear terminal or cmd window"""
-   # for windows
-   if name == 'nt':
+    """clear terminal or cmd window"""
+    # for windows
+    if name == 'nt':
         system('cls')
 
-   # for mac and linux
-   else:
-    system('clear')
+    # for mac and linux
+    else:
+        system('clear')
 
 def intro():
     """show intro before game start"""
@@ -18,15 +18,27 @@ def intro():
     sleep(2)
 
     clear()
-    pass
+    
+def take_input():
+    while True :
 
+        player_choice = int(input("enter number (0 , 1 , 2) : "))
+        
+        if player_choice not in (0,1,2) :
+            print("incorrect input /\ try again .")
+        else :
+            break
+    cpu_choice = randint(3,5)
+
+    return (player_choice , cpu_choice)
 
 def check(p , c):
     """check who win"""
 
+    clear()
     val = ["rock" , "paper" , "scissors"] #available choices
-    print(f'player choice is : {val[p]} --> {p} \ncpu choice is : {val[c-3]} --> {c}') #print choices
-    
+    print(f'player choice is : {val[p]} --> {p} \ncpu choice is : {val[c-3]} --> {c}\n\n') #print choices
+    sleep(1)
     res = abs(p-c)
 
     #to understand conditions see excel file
@@ -39,14 +51,14 @@ def check(p , c):
     elif res in (4,1):
         print("winner is cpu !!")
 
+    print("\n\nnext round ...")
+    sleep(2.5)
+    clear()
 
 #main 
 intro()
 for i in range(10):
 
+    p_c_choice = take_input()
 
-    player_choice = int(input("enter number (0 , 1 , 2) : "))
-
-    cpu_choice = randint(3,5)
-
-    check(player_choice , cpu_choice)
+    check(p_c_choice[0] , p_c_choice[1] )
